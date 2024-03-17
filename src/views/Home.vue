f8580844260f1e96f9d4b13a9c2bd1f7cbdf4fe3
wuxs <317009160@qq.com>
2024-03-17 11:22:51 +0800

    feat:修复因父盒子相对定位，子盒子定位问题

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -80,6 +80,10 @@ onUnmounted(() => {
   }
 
   .contain {
+    position: absolute;
+    height: 100%;
+    left: 50%;
+    transform: translateX(-50%);
     .title {
       font-size: 80px;
       font-family: 'stxingka';
